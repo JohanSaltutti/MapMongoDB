7ec645426ffb31973a1b617338d26070191a90ba
zink anthony <anthony.zink@outlook.fr>
2018-11-30 09:45:23 +0100

    index

--- index.php
+++ index.php
@@ -18,12 +18,18 @@
     use \Psr\Http\Message\ResponseInterface as Response;
     use app\helpers\CurlClass;
 
-    $app = new \Slim\App;
-    $curl = CurlClass::getInstance();
+    $app = new \Slim\App([
+        'settings' => [
+            'displayErrorDetails' => true,
+        ],
+    ]);
 
     $app->get('/', function (Request $request, Response $response, array $args) {
-        $response->getBody()->write("Hello");
-        return $response;
+        $curl = CurlClass::get('https://geoservices.grand-nancy.org/arcgis/rest/services/public/VOIRIE_Parking/MapServer/0/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=nom%2Cadresse%2Cplaces%2Ccapacite&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&queryByDistance=&returnExtentsOnly=false&datumTransformation=&parameterValues=&rangeValues=&f=pjson');
+
+        if ($curl['code'] == 200) {
+            var_dump($curl['response']['features']);
+        }
     });
 
     $app->run();
