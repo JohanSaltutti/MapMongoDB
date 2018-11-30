8d05262184721a629255ed9d485da607c4916210
Johan SALTUTTI <jo.saltutti@orange.fr>
2018-11-30 11:35:13 +0100

    Ajout maps google avec affichage markers

--- index.php
+++ index.php
@@ -22,14 +22,18 @@
         'settings' => [
             'displayErrorDetails' => true,
         ],
-    ]);
+    ]);   
 
     $app->get('/', function (Request $request, Response $response, array $args) {
-        $curl = CurlClass::get('https://geoservices.grand-nancy.org/arcgis/rest/services/public/VOIRIE_Parking/MapServer/0/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=nom%2Cadresse%2Cplaces%2Ccapacite&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&queryByDistance=&returnExtentsOnly=false&datumTransformation=&parameterValues=&rangeValues=&f=pjson');
+        $file = 'views/map.html';
+        if (file_exists($file))
+            return $response->write(file_get_contents($file));
+    });
 
-        if ($curl['code'] == 200) {
-            var_dump($curl['response']['features']);
-        }
+    $app->get('/ajax/markers', function (Request $request, Response $response, array $args) {
+        $curl = CurlClass::get('https://geoservices.grand-nancy.org/arcgis/rest/services/public/VOIRIE_Parking/MapServer/0/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=nom%2Cadresse%2Cplaces%2Ccapacite&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&queryByDistance=&returnExtentsOnly=false&datumTransformation=&parameterValues=&rangeValues=&f=pjson');
+        if ($curl['code'] == 200)
+            return $response->withJson($curl['response']['features']);
     });
 
     $app->run();
