ffdf4454a63ae6325bcc10a63c7f892b944204e3
Johan SALTUTTI <jo.saltutti@orange.fr>
2018-11-30 09:27:08 +0100

    Ajout get curl

--- helpers/CurlClass.php
+++ helpers/CurlClass.php
@@ -7,10 +7,19 @@ use \Curl\Curl;
 class CurlClass{
     private static $instance = null;
 
-    public static function getInstance(){
+    private static function getInstance(){
         if(self::$instance == null)
             self::$instance = new Curl();
 
         return self::$instance;
+    }
+
+    public static function get($url){
+        $curl = self::getInstance();
+        $curl->get($url);
+        return [
+            'code'     => $curl->error ? $curl->errorCode    : 200,
+            'response' => $curl->error ? $curl->errorMessage : $curl->response,
+        ];
     }
 }
