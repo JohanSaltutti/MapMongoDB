66906ceb4539a212628aa792d484d4ad3c9326c4
JesterHeads <benjamin.thouvenin.54220@gmail.com>
2019-01-14 22:26:09 +0100

    ajout des parkings et lieux touristiques en bases mongo

--- index.php
+++ index.php
@@ -1,22 +1,39 @@
 <?php
-	/*$manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
-
-	$bulk = new MongoDB\Driver\BulkWrite;
-
-    $bulk->insert([
-        'test' => 'test2'
-    ]);
-
-    $writeConcern = new MongoDB\Driver\WriteConcern(MongoDB\Driver\WriteConcern::MAJORITY, 1000);
-    $result       = $manager->executeBulkWrite('testeeee.colle1', $bulk, $writeConcern);
-
-    var_dump($result);*/
-
     require './vendor/autoload.php';
 
     use \Psr\Http\Message\ServerRequestInterface as Request;
     use \Psr\Http\Message\ResponseInterface as Response;
     use app\helpers\CurlClass;
+
+    $manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+
+	$bulk = new MongoDB\Driver\BulkWrite;
+
+    $writeConcern = new MongoDB\Driver\WriteConcern(MongoDB\Driver\WriteConcern::MAJORITY, 1000);
+    $curl = CurlClass::get('https://geoservices.grand-nancy.org/arcgis/rest/services/public/VOIRIE_Parking/MapServer/0/query?where=1%3D1&text=&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=nom%2Cadresse%2Cplaces%2Ccapacite&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&queryByDistance=&returnExtentsOnly=false&datumTransformation=&parameterValues=&rangeValues=&f=pjson');
+    
+    if ($curl['code'] == 200){
+        $bulk->delete([]);
+        foreach($curl['response']['features'] as $parking){
+            if($parking['attributes']['PLACES'] == NULL)$parking['attributes']['PLACES'] = 0;
+            $parking['attributes']['positions'] = $parking['geometry'];
+            $bulk->insert($parking['attributes']);
+        }
+        $manager->executeBulkWrite('mapMongo.parking',$bulk);
+    }
+
+    $curl = CurlClass::get('https://geoservices.grand-nancy.org/arcgis/rest/services/public/DOPUB_Tourisme/MapServer/0/query?where=COMMUNE%3D%27Nancy%27&text=&objectIds=&time=&geometry=&geometryType=esriGeometryPolygon&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=NAME%2CDESCRIPTION%2CCOMMUNE%2CADRESSE%2CTYPE%2CLINK%2CPHOTOS&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=4326&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&queryByDistance=&returnExtentsOnly=false&datumTransformation=&parameterValues=&rangeValues=&f=pjson');
+    $bulk = new MongoDB\Driver\BulkWrite;
+
+    if ($curl['code'] == 200){
+        $bulk->delete([]);
+        foreach($curl['response']['features'] as $lieu){
+            $lieu['attributes']['positions'] = $lieu['geometry'];
+            $bulk->insert($lieu['attributes']);
+        }
+        $manager->executeBulkWrite('mapMongo.lieuTouristique',$bulk);
+    }
+
 
     $app = new \Slim\App([
         'settings' => [
